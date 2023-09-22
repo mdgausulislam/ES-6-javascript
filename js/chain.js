3cee3c30632f49c0620d811bd3eb39bf43bb22ae
unknown <gausulislam5@gmail.com>
2023-09-22 23:37:43 +0600

    update chain

---
+++ js/chain.js
@@ -0,0 +1,13 @@
+const users=[
+    {id:1,name:'abul',job:'doctor'}
+];
+console.log(users[0].name);
+
+
+const data={
+    count:5000,
+    data:[{id:1,name:'babul',job:'leader'},
+{id:2,name:'dabul',job:'leader2'}]
+}
+
+console.log(data.data[0].job);
