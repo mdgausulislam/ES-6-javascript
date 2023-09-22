22f0c5a8680d19f890804c4c17a0e1931b191437
unknown <gausulislam5@gmail.com>
2023-09-22 12:10:25 +0600

    first commit

---
+++ js/let-const.js
@@ -0,0 +1,10 @@
+// let: let it to reassign
+// const: don not allow to reassign
+let money=25;
+money=35;
+console.log(money);
+
+const numbers=[12,89,90,67];
+numbers.push(123);
+numbers[1]=50;
+console.log(numbers);
