470cefda9e4ce7c22c0b1a3bf45de6ad920a872e
unknown <gausulislam5@gmail.com>
2023-09-22 21:25:56 +0600

    update section

---
+++ js/destructuring.js
@@ -0,0 +1,30 @@
+const fish={
+    name:'king hilsha',
+    address: 'chadpur',
+    color: 'silver',
+    phone:'01785003901',
+    price: 4000
+};
+// const phone =fish.phone;
+// const color=fish.color;
+// const price=fish.price;
+
+const {phone}=fish;
+
+console.log(phone);
+console.log(phone);
+console.log(phone);
+console.log(phone);
+console.log(phone);
+
+const {age,name}={name:'hemonto',age:24,profession:'cse'};
+
+console.log(age);
+console.log(name);
+
+
+// array destructuring
+
+const [a,b]=[44,99];
+console.log(a);
+
