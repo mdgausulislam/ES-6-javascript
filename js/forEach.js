8ac0f8b3f4ff94240b9b159be92e64db2adcfe1f
unknown <gausulislam5@gmail.com>
2023-09-23 00:46:12 +0600

    for each update

---
+++ js/forEach.js
@@ -0,0 +1,15 @@
+const friends=['tom hanks','tom cruise','tom brady','tom solaiman'];
+
+const friendsList=friends.forEach(friend=>friend[1]);
+console.log(friendsList);
+
+
+const products=[
+    {id:1, name:'laptop',price:45000},
+    {id:2, name:'mobile',price:3000},
+    {id:3, name:'watch',price:4000},
+    {is:4, name:'tablet',price:4000},
+]
+
+
+
--- js/map.js
+++ js/map.js
@@ -21,3 +21,29 @@ console.log(result);
 
 const double=numbers.map(num=>num*2)
 console.log(double);
+
+
+const numbers1=[12,56,87,44];
+const divided=numbers1.map(n=>n/2);
+console.log(divided);
+
+const divided1=numbers1.map(n=>n/3);
+console.log(divided1);
+
+
+const friends=['tom hanks','tom cruise','tom brady','tom solaiman'];
+const firstLetter=friends.map(f=>f[0]);
+console.log(firstLetter);
+
+const nameLength=friends.map(f=>f.length);
+console.log(nameLength);
+
+
+const products=[
+    {id:1, name:'laptop',price:45000},
+    {id:2, name:'mobile',price:3000},
+    {id:3, name:'watch',price:4000},
+    {is:4, name:'tablet',price:4000},
+]
+const nameList=products.map(n=>n.name);
+console.log(nameList);
