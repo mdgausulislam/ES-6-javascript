8f67e4e9b2fb28c891eb62a8792d8492c2355ea3
unknown <gausulislam5@gmail.com>
2023-09-22 19:59:55 +0600

    update arrow function

---
+++ js/arrow-function.js
@@ -0,0 +1,41 @@
+// FUNCTION declaration
+// function add (first,second){
+//     const total=first+second;
+//     return total;
+// }
+
+// const add1=function add1(first,second){
+//     const total=first+second;
+//     return total;
+// }
+
+//function expression
+// const add1=function add1(first,second){
+//     const total=first+second;
+//     return total;
+// }
+
+
+function add (first,second){
+    const total=first+second;
+    return total;
+}
+
+function add4(first,second){
+    return first+second;
+}
+
+
+const add5=function(first,second){
+    return first+second;
+}
+
+
+//arrow function
+const add6 =(first,second)=> first+second;
+
+
+const result=add6(20,30);
+console.log(result);
+
+
