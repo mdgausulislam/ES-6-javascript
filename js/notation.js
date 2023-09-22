d6c15334a6b3a0d545361ea80236294bedecdd31
unknown <gausulislam5@gmail.com>
2023-09-23 01:28:45 +0600

    update notation and reduce]

---
+++ js/notation.js
@@ -0,0 +1,14 @@
+const  student ={
+    name:'hemonto',
+    age:24,
+    class:'bsc Engineering',
+    marks:{
+        math:78,
+        physics:58,
+        chemistry:50
+    }
+}
+const marks=student.marks;
+const marks1=student.marks.math;
+console.log(marks);
+console.log(marks1);
---
+++ js/reduce.js
@@ -0,0 +1,3 @@
+const number=[1,2,3,4,5];
+const total=number.reduce((previous,current)=>previous+current,0);
+console.log(total);
